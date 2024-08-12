0fd13a6c61d9a5cb75158972025ddf88cd443f57
Genesis Blockhead <genesisblockhead@protonmail.com>
2024-08-12 17:09:15 -0400

    add a database

--- app.js
+++ app.js
@@ -1,50 +1,95 @@
-let passport = require('passport');
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let crypto = require("crypto");
+let passport = require("passport");
+let createError = require("http-errors");
+let express = require("express");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
 
-var app = express();
+let indexRouter = require("./routes/index");
+let usersRouter = require("./routes/users");
+
+let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/login', (_, res) => res.render('login'))
+app.get("/login", (_, res) => res.render("login"));
 
-app.post('/login/password',
-  passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
-  function(req, res) {
-    res.redirect('/dashboard');
-  });
+var LocalStrategy = require("passport-local");
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var strategy = new LocalStrategy(function verify(username, password, cb) {
+  db.get(
+    "SELECT * FROM users WHERE username = ?",
+    [username],
+    function (err, user) {
+      if (err) {
+        return cb(err);
+      }
+      if (!user) {
+        return cb(null, false, { message: "Incorrect username or password." });
+      }
+
+      crypto.pbkdf2(
+        password,
+        user.salt,
+        310000,
+        32,
+        "sha256",
+        function (err, hashedPassword) {
+          if (err) {
+            return cb(err);
+          }
+          if (!crypto.timingSafeEqual(user.hashed_password, hashedPassword)) {
+            return cb(null, false, {
+              message: "Incorrect username or password.",
+            });
+          }
+          return cb(null, user);
+        },
+      );
+    },
+  );
+});
+
+app.use(strategy);
+
+app.post(
+  "/login/password",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureMessage: true,
+  }),
+  function (req, res) {
+    res.redirect("/dashboard");
+  },
+);
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
