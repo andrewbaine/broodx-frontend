6d3f69949a402a0c034c443d14e82901628304e2
Genesis Blockhead <genesisblockhead@protonmail.com>
2024-08-12 13:11:22 -0400

    add login route

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/login', (_, res) => res.render('login'))
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
