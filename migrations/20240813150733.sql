bf7df00eb36c6e00b04ee848546fc746f6546adb
Genesis Blockhead <genesisblockhead@protonmail.com>
2024-08-13 12:48:45 -0400

    atlas migrate hash

--- migrations/20240813150733.sql
+++ migrations/20240813150733.sql
@@ -2,6 +2,5 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   email TEXT NOT NULL UNIQUE,
   hashed_password TEXT,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
-  junk int
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW ()
 );
